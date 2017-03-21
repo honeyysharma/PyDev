96f3480f0298b0786bb97e5297d518e16c92e464
honeyysharma <honey.y.sharma@gmail.com>
2017-03-21 23:20:17 +0530

    Updated with explanation.

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,19 +1,29 @@
-#binary gap problem (CODILITY)
 def binaryGap(n):
     gap = 0
     binary_gap = 0
-    x = str(bin(n)).strip("0b")
-#     print int('1001000100000001',2)
-    list = map(int,str(x))
-    for i,b in enumerate(list):
+    x = bin(n)[2:].zfill(8)
+    #print int('1001000',2)
+    #print x
+    list = map(int, str(x))
+    for i, b in enumerate(list):
         if b == 0:
-            gap = gap+1
+            gap = gap + 1
         if b == 1:
-            if list[i-gap-1] and i-gap-1 != -1 and binary_gap < gap:
+            # list[i-gap-1] == 1 checks if the gap is having 1 on the left too
+            # i-gap-1 != -1 checks if the index is becoming less than 0
+            # 0 0 0 0 1 0 0 1 ---- input
+            # 0 1 2 3 4 5 6 7 ---- indices
+            # when index = 4; gap = 4; So i - gap -1 = -1; It is less than zero and list[-1] = list[7]
+            # when index = 7; gap = 2; So i - gap -1 = 4; It is not less than zero and list[4] = 1
+            if list[i - gap - 1] == 1 and i - gap - 1 != -1 and binary_gap < gap:
                 binary_gap = gap
             gap = 0
 
     return binary_gap
-                
 
-print binaryGap(37121)
+
+print ("binary gap: %d" % binaryGap(72))
+print ("binary gap: %d" % binaryGap(9))
+print ("binary gap: %d" % binaryGap(529))
+print ("binary gap: %d" % binaryGap(20))
+print ("binary gap: %d" % binaryGap(15))
